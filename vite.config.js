00c8bd58e9c4f11ed008f30900890deb3b1302dd
Melissa Balsante <mibsbalsante@gmail.com>
2023-02-24 03:07:59 -0300

    chore(dependencies): install sass and redux

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: '@import ";',
+        additionalData: "",
       },
     },
   },
@@ -17,10 +17,11 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
       "@ast": fileURLToPath(new URL("./src/assets", import.meta.url)),
       "@cmp": fileURLToPath(new URL("./src/components", import.meta.url)),
+      "@str": fileURLToPath(new URL("./src/store", import.meta.url)),
       "@utl": fileURLToPath(new URL("./src/utils", import.meta.url)),
     },
   },
   server: {
     port: 9000,
   },
-});
+})
