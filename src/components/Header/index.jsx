8a198ef610c0dc7652c2e3ffa9c902b42ce6e681
Melissa Balsante <mibsbalsante@gmail.com>
2023-02-26 15:05:58 -0300

    feat(layout): use ui5 components with header and make it top-fixed

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,20 @@
 import { NavLink } from "react-router-dom"
+import { Title } from "@ui5/webcomponents-react"
 
 import Search from "@cmp/Search"
 
+import styles from "./styles.module.scss"
+
 const Header = () => (
-  <header>
-    <NavLink to="/">
-      <h1>Movies Database</h1>
-    </NavLink>
-    <Search />
+  <header className={styles.header}>
+    <div className={styles.headerContainer}>
+      <NavLink to="/" className={styles.headerLink}>
+        <Title level="H1" className={styles.headerTitle}>
+          Movies Database
+        </Title>
+      </NavLink>
+      <Search />
+    </div>
   </header>
 )
 
