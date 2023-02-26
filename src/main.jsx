2f2606523017ae109681936c3ba2e32cc399d5fd
Melissa Balsante <mibsbalsante@gmail.com>
2023-02-26 14:39:27 -0300

    feat(styling): use sap horizon as template and set its variables to project variables
    this pr also includes scss mixins for containers

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,14 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
 import { RouterProvider } from "react-router-dom"
+
 import { ThemeProvider } from "@ui5/webcomponents-react"
+import { setTheme } from "@ui5/webcomponents-base/dist/config/Theme"
+
+import "@ui5/webcomponents/dist/features/InputElementsFormSupport.js"
+import "@ui5/webcomponents-react/dist/Assets"
+
+setTheme("sap_horizon")
 
 import router from "./router"
 import store from "./store"
