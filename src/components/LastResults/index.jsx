b2535a2a80324f304396c80e5c3bd7b9865835e2
Melissa Balsante <mibsbalsante@gmail.com>
2023-02-27 03:18:31 -0300

    feat(styling): improve latest searches list results

--- src/components/LastResults/index.jsx
+++ src/components/LastResults/index.jsx
@@ -2,21 +2,27 @@ import { NavLink } from "react-router-dom"
 import { useSelector } from "react-redux"
 import { Title } from "@ui5/webcomponents-react"
 
+import styles from "./styles.module.scss"
+
 const LastResults = () => {
   const moviesList = useSelector(state => [...state.movie.moviesList].reverse())
 
   return (
     moviesList.length > 0 && (
-      <section>
+      <section className={styles.nav}>
         <Title level="H3">Latest searches</Title>
 
-        <div>
+        <nav className={styles.navList}>
           {moviesList.map(({ imdbID, Title }) => (
-            <NavLink key={imdbID} to={`${Title.toLowerCase()}`}>
-              {Title}
+            <NavLink
+              className={styles.navLink}
+              key={imdbID}
+              to={`${Title.toLowerCase()}`}
+            >
+              <span>{Title}</span>
             </NavLink>
           ))}
-        </div>
+        </nav>
       </section>
     )
   )
